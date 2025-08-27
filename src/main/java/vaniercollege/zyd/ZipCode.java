629f8e88d2f0d27972af3505c7d66dc99efeb0d2
Z.Y.D. | "Cloumy <2480549@edu.vaniercollege.qc.ca>
2025-08-27 11:39:51 -0400

    feat: Constructors for Zip

--- src/main/java/vaniercollege/zyd/ZipCode.java
+++ src/main/java/vaniercollege/zyd/ZipCode.java
@@ -1,9 +1,28 @@
 package vaniercollege.zyd;
 
+import java.util.Arrays;
+
 /**
  * @author 2480549
  * 2025/08/27
+ * https://github.com/Cloumy074/Lab01_F25_ZipCode.git
  */
 class ZipCode {
+    int Zip;
+
+    public ZipCode(int input) {
+        String temp = Integer.toString(input);
+        System.out.println(temp);
+    }
+
+    public ZipCode(String input) {}
+
+    public String GetBarCode() {
+        return "";
+    }
+    
+    private int parseBarCode() {
+        return Zip;
+    }
     
 }
