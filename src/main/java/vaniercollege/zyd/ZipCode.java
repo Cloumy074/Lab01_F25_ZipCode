409b3654525ee887261d7e3096f84ec2b222c43d
Z.Y.D. | "Cloumy <2480549@edu.vaniercollege.qc.ca>
2025-08-27 11:58:01 -0400

    fix: barcode test

--- src/main/java/vaniercollege/zyd/ZipCode.java
+++ src/main/java/vaniercollege/zyd/ZipCode.java
@@ -12,10 +12,27 @@ class ZipCode {
 
     public ZipCode(int input) {
         String temp = Integer.toString(input);
-        System.out.println(temp);
+        
+        if (temp.length() > 5) {
+            System.out.println(temp + " zip code is more than 5 digits");
+        } else {
+            this.Zip = input;
+        }
     }
 
-    public ZipCode(String input) {}
+    public ZipCode(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != '0' && input.charAt(i) != '1' ) {
+                System.out.println("bar code character " + input.charAt(i) + " must be '0' or '1'");
+            }
+        }
+        if (input.lastIndexOf('1') != (input.length() - 1) || input.indexOf('1') != 0) {
+            System.out.println("Error: bar code missing a 1 at start or end");
+        }
+        if ((input.length() - 2) % 5 != 0) {
+            System.out.println("Error: bar code must be in multiples of 5-binary digits");
+        }
+    }
 
     public String GetBarCode() {
         return "";
