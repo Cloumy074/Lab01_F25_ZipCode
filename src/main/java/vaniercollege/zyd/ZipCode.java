391e97b373d15cd765c173cb062eb20c504cac71
Zhang Y.D. <me@cloumy.top>
2025-08-28 14:53:34 -0400

    format: clean unnecessary code

--- src/main/java/vaniercollege/zyd/ZipCode.java
+++ src/main/java/vaniercollege/zyd/ZipCode.java
@@ -2,7 +2,7 @@ package vaniercollege.zyd;
 
 /**
  * @author 2480549
- * 2025/08/27
+ * 2025/08/27 - /08/28
  * https://github.com/Cloumy074/Lab01_F25_ZipCode.git
  */
 class ZipCode {
@@ -10,30 +10,32 @@ class ZipCode {
 
     public ZipCode(int input) {
         String temp = Integer.toString(input);
+        this.Zip = input;
 
         if (temp.length() > 5) {
             System.out.println(temp + " zip code is more than 5 digits");
-        } else {
-            this.Zip = input;
         }
     }
 
     public ZipCode(String input) {
         for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) != '0' && input.charAt(i) != '1' ) {
+            if (input.charAt(i) != '0' && input.charAt(i) != '1') {
                 System.out.println("bar code character " + input.charAt(i) + " must be '0' or '1'");
             }
         }
+
         if (input.lastIndexOf('1') != (input.length() - 1) || input.indexOf('1') != 0) {
             System.out.println("Error: bar code missing a 1 at start or end");
         }
+
         if ((input.length() - 2) % 5 != 0) {
             System.out.println("Error: bar code must be in multiples of 5-binary digits");
         }
+
         int lastDigit = (input.length() - 1);
         String barcode = input.substring(1, lastDigit);
-        for (int i = 4; i < barcode.length(); i+=5) {
-            String temp = barcode.substring(i - 4, i);
+        for (int i = 4; i < barcode.length(); i += 5) {
+            String temp = barcode.substring(i - 4, i + 1);
             int count = 0;
             for (int j = 0; j < temp.length(); j++) {
                 if (temp.charAt(j) == '1') {
@@ -44,7 +46,6 @@ class ZipCode {
                 System.out.println(temp + " is an invalid sequence in the bar code");
             }
         }
-        
         this.Zip = parseBarCode(barcode);
     }
 
@@ -52,33 +53,29 @@ class ZipCode {
         int zipCode = this.Zip;
         int lastDigit = zipCode % 10;
         String barCode = "";
+
         for (int i = 1; i <= 5; i++) {
             switch (lastDigit) {
-                case 1 -> barCode = "00011" +  barCode;
-                case 2 -> barCode = "00101" +  barCode;
-                case 3 -> barCode = "00110" +  barCode;
-                case 4 -> barCode = "01001" +  barCode;
-                case 5 -> barCode = "01010" +  barCode;
-                case 6 -> barCode = "01100" +  barCode;
-                case 7 -> barCode = "10001" +  barCode;
-                case 8 -> barCode = "10010" +  barCode;
-                case 9 -> barCode = "10100" +  barCode;
-                case 0 -> barCode = "11000" +  barCode;
+                case 1 -> barCode = "00011" + barCode;
+                case 2 -> barCode = "00101" + barCode;
+                case 3 -> barCode = "00110" + barCode;
+                case 4 -> barCode = "01001" + barCode;
+                case 5 -> barCode = "01010" + barCode;
+                case 6 -> barCode = "01100" + barCode;
+                case 7 -> barCode = "10001" + barCode;
+                case 8 -> barCode = "10010" + barCode;
+                case 9 -> barCode = "10100" + barCode;
+                case 0 -> barCode = "11000" + barCode;
             }
             zipCode /= 10;
             lastDigit = zipCode % 10;
         }
-        int currentDigit = barCode.length() / 5;
-        int missingDigit = 5 - currentDigit;
-        String missingZero = "";
-        for (int i = 0; i < missingDigit; i++) {
-            missingZero += "11000";
-        }
-        return String.format("1%s%s1", missingZero, barCode);
+        return String.format("1%s1", barCode);
     }
-    
+
     private int parseBarCode(String input) {
         String result = "";
+
         for (int i = 4; i < input.length(); i += 5) {
             String temp = input.substring(i - 4, i + 1);
             switch (temp) {
@@ -96,5 +93,4 @@ class ZipCode {
         }
         return Integer.parseInt(result);
     }
-    
 }
